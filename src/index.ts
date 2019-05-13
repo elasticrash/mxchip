1e741da81c7e8d379fdad76435e8e826c0714946
stefano <s.kouroupis@gmail.com>
2019-05-14 00:16:21 +0100

    get function and angular empty frontend

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 import * as http from 'http';
 import * as sqlite3 from 'sqlite3';
+import * as url from 'url';
 
+const URLSearchParams = url.URLSearchParams;
 const hostname = '0.0.0.0';
 const sqlite = sqlite3.verbose();
 const port = 3000;
 var db = new sqlite.Database('./timedb.sqlite');
 
-http.createServer((req, res) => {
+http.createServer((req: any, res: any) => {
     if (req.method === 'POST') {
         let body = '';
-        req.on('data', chunk => {
+        req.on('data', (chunk: any) => {
             body += chunk.toString();
         });
         req.on('end', () => {
             try {
                 db.serialize(() => {
                     const data = JSON.parse(body);
-                    db.run("INSERT INTO TimeSeries (temperature, humidity, date, location) VALUES (?, ? ,?, ?)", [
+                    db.run("INSERT INTO TimeSeries (temperature, humidity, date, location) VALUES (?, ? ,?, ?, ?)", [
                         data.temperature,
                         data.humidity,
                         new Date().toUTCString(),
-                        data.location
+                        data.location,
+                        Date.now() / 1000 | 0
                     ]);
                 });
             } catch (error) {
@@ -29,6 +32,15 @@ http.createServer((req, res) => {
             console.log(new Date().toUTCString());
             console.log(body);
             res.end('ok');
+        });
+    } else if (req.method === 'GET') {
+        const search_params = new URLSearchParams(req.url.split('?')[1]);
+        const from = search_params.get('from');
+        const to = search_params.get('to');
+        db.serialize(() => {
+            db.all("SELECT * FROM TimeSeries WHERE timestamp > ? AND timestamp < ?", [from, to], (err, rows) => {
+                res.end(JSON.stringify(rows));
+            });
         });
     }
 }).listen(port, hostname, () => {
